50616b76315d7870d144a8f766ec060af6fa4168
Gustavo Passos <theguspassos@gmail.com>
2021-01-09 22:10:19 -0300

    add basic character controller

---
+++ Assets/Controllers/InputHandler.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Physics;
+using UnityEngine;
+
+namespace Assets.Controllers
+{
+    public class InputHandler : MonoBehaviour
+    {
+        [SerializeField] private Mover mover;
+
+        private void Update()
+        {
+            var x = Input.GetAxisRaw("Horizontal");
+            var y = Input.GetAxisRaw("Vertical");
+
+            mover.SetXInput(x);
+            mover.SetYInput(y);
+        }
+    }
+}
---
+++ Assets/Physics/Mover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Physics
+{
+    public class Mover : MonoBehaviour
+    {
+        Vector2 currentVelocity;
+
+        float xInput;
+        float yInput;
+
+        [SerializeField] float moveSpeed;
+        [SerializeField] LayerMask collisionMask;
+
+        public void SetXInput(float xInput) => this.xInput = xInput;
+        public void SetYInput(float yInput) => this.yInput = yInput;
+
+        void FixedUpdate()
+        {
+            currentVelocity.x = xInput * moveSpeed;
+            currentVelocity.y = yInput * moveSpeed;
+
+            var velocity = currentVelocity * Time.fixedDeltaTime;
+
+            transform.Translate(velocity);
+        }
+    }
+}
