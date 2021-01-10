939744e7d577e0f626ca54cbd1f7a4d5551fb092
Gustavo Passos <theguspassos@gmail.com>
2021-01-09 23:00:03 -0300

    add doors

---
+++ Assets/Interactables/Door.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Interactables
+{
+    public class Door : MonoBehaviour
+    {
+        GameObject door;
+        bool canOpen;
+
+        private void Awake()
+        {
+            door = transform.GetChild(0).gameObject;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            canOpen = true;
+        }
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            canOpen = false;
+        }
+
+        private void Update()
+        {
+            Debug.Log(canOpen);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                AstarPath.active.UpdateGraphs(door.GetComponent<BoxCollider2D>().bounds);
+                door.SetActive(!door.activeSelf);
+            }
+        }
+    }
+}
