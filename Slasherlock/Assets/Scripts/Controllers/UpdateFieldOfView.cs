f1c2ae8567ea986ee6eac33d2402e3e74cdc0e2a
Gustavo Passos <theguspassos@gmail.com>
2021-01-10 22:18:25 -0300

    add fov

---
+++ Slasherlock/Assets/Scripts/Controllers/UpdateFieldOfView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class UpdateFieldOfView : MonoBehaviour
+    {
+        [SerializeField] FieldOfView fieldOfView;
+
+        private void Update()
+        {
+            fieldOfView.SetOrigin(transform.position);
+        }
+    }
+}
---
+++ Slasherlock/Assets/Scripts/Fov/FieldOfView.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class FieldOfView : MonoBehaviour {
+
+    [SerializeField] private LayerMask layerMask;
+    private Mesh mesh;
+    public float fov;
+    public float viewDistance;
+    private Vector3 origin;
+    private float startingAngle;
+
+    private void Start() {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        origin = transform.position;
+    }
+
+    private void LateUpdate() {
+        int rayCount = 50;
+        float angle = startingAngle;
+        float angleIncrease = fov / rayCount;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++) {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+            Debug.DrawRay(origin, GetVectorFromAngle(angle) * 100, Color.red);
+            if (raycastHit2D.collider == null) {
+                // No hit
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            } else {
+                // Hit object
+                Debug.Log(raycastHit2D.collider.name);
+                vertex = raycastHit2D.point;
+            }
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0) {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
+    }
+
+    public void SetOrigin(Vector3 origin) {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection) {
+        startingAngle = GetAngleFromVectorFloat(aimDirection) + fov / 2f;
+    }
+
+    public void SetFoV(float fov) {
+        this.fov = fov;
+    }
+
+    public void SetViewDistance(float viewDistance) {
+        this.viewDistance = viewDistance;
+    }
+
+    public static float GetAngleFromVectorFloat(Vector3 dir) {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+
+    public static Vector3 GetVectorFromAngle(float angle) {
+        // angle = 0 -> 360
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+}
+
