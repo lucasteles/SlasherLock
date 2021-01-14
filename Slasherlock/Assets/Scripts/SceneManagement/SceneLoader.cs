2e8c4860a0725cabb274e0f3d6f8ef4483c4f6e1
Gustavo Passos <theguspassos@gmail.com>
2021-01-13 22:10:28 -0300

    add level loader with transitions

--- Slasherlock/Assets/Scripts/SceneManagement/SceneLoader.cs
+++ Slasherlock/Assets/Scripts/SceneManagement/SceneLoader.cs
@@ -1,8 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.SceneManagement
 {
     public class SceneLoader : MonoBehaviour
     {
+        public static SceneLoader Instance { get; private set; }
+
+        [SerializeField] GameObject objectWithTransition;
+        [SerializeField] float transitionTime;
+        Animator transition;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+
+                var transitionInScreen = Instantiate(objectWithTransition);
+                transition = transitionInScreen.GetComponentInChildren<Animator>();
+
+                return;
+            }
+
+            Destroy(gameObject);
+        }
+
+        public void LoadLevel(string level)
+            => StartCoroutine(LoadLevelAsync(level));
+
+        IEnumerator LoadLevelAsync(string level)
+        {
+            transition.SetTrigger("start");
+
+            yield return new WaitForSeconds(transitionTime);
+
+            SceneManager.LoadScene(level);
+        }
     }
 }
