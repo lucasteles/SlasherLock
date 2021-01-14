915b4265fd9330def9006af21285027332c43c9c
Gustavo Passos <theguspassos@gmail.com>
2021-01-13 22:22:53 -0300

    add character killer

---
+++ Slasherlock/Assets/Scripts/Characters/MainCharacter/MainCharacterKiller.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.Physics;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Characters.MainCharacter
+{
+    public class MainCharacterKiller : MonoBehaviour
+    {
+        Mover mover;
+        SpriteRenderer spriteRenderer;
+        [SerializeField] float timeToKill;
+
+        void Awake()
+        {
+            mover = GetComponent<Mover>();
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Kill();
+        }
+
+        public void Kill()
+        {
+            mover.PreventMovement();
+            StartCoroutine(DestroyAfterTime());
+        }
+
+        IEnumerator DestroyAfterTime()
+        {
+            yield return new WaitForSeconds(timeToKill);
+            spriteRenderer.enabled = false;
+        }
+    }
+}
--- Slasherlock/Assets/Scripts/Physics/Mover.cs
+++ Slasherlock/Assets/Scripts/Physics/Mover.cs
@@ -5,16 +5,23 @@ namespace Assets.Scripts.Physics
     public class Mover : MonoBehaviour
     {
         Vector2 currentVelocity;
-
+        bool canMove;
         float xInput;
         float yInput;
 
         [SerializeField] float moveSpeed;
         Rigidbody2D rb;
 
-        public void SetXInput(float xInput) => this.xInput = xInput;
+        public void AllowMovement() => canMove = true;
+        public void PreventMovement()
+        {
+            StopInput();
+            canMove = false;
+        }
 
+        public void SetXInput(float xInput) => this.xInput = xInput;
         public void SetYInput(float yInput) => this.yInput = yInput;
+
         public void StopInput()
         {
             xInput = 0;
@@ -28,6 +35,8 @@ namespace Assets.Scripts.Physics
 
         void FixedUpdate()
         {
+            if (!canMove) return;
+
             currentVelocity.x = xInput * moveSpeed;
             currentVelocity.y = yInput * moveSpeed;
 
