5dff86dc20c8a5ad797f8a43b4680dcf5180d166
Lucas Teles <lucas.teles@outlook.com>
2021-01-15 11:07:35 -0300

    add come door sounds

--- Slasherlock/Assets/Scripts/Interactables/Door.cs
+++ Slasherlock/Assets/Scripts/Interactables/Door.cs
@@ -4,12 +4,19 @@ namespace Assets.Interactables.Physics
 {
     public class Door : MonoBehaviour
     {
+        [SerializeField]AudioClip open;
+        [SerializeField]AudioClip close;
+        AudioSource audioSource;
         GameObject door;
         bool canOpen;
+
+        float timeToAutoClose = 0.5f;
+        float autoCloseTimer = 0f;
 
         private void Awake()
         {
             door = transform.GetChild(0).gameObject;
+            audioSource = GetComponent<AudioSource>();
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -20,13 +27,45 @@ namespace Assets.Interactables.Physics
         void OnTriggerExit2D(Collider2D other)
         {
             canOpen = false;
+            Invoke(nameof(AutoCloseDoor), timeToAutoClose);
         }
 
-        private void Update()
+        void AutoCloseDoor()
         {
-            if (!Input.GetKeyDown(KeyCode.Space)) return;
-            AstarPath.active.UpdateGraphs(door.GetComponent<BoxCollider2D>().bounds);
-            door.SetActive(!door.activeSelf);
+            if (!canOpen)
+                CloseDoor();
+        }
+
+        void CloseDoor()
+        {
+            if (!door.activeSelf)
+            {
+                door.SetActive(true);
+                audioSource.PlayOneShot(close);
+                UpdatePath();
+            }
+        }
+
+        void OpenDoor()
+        {
+            if (door.activeSelf)
+            {
+                door.SetActive(false);
+                audioSource.PlayOneShot(open);
+                UpdatePath();
+            }
+        }
+
+        void UpdatePath() => AstarPath.active.UpdateGraphs(door.GetComponent<BoxCollider2D>().bounds);
+        void Update()
+        {
+            if (!Input.GetKeyDown(KeyCode.Space) || !canOpen) return;
+
+            if (door.activeSelf)
+                OpenDoor();
+            else
+                CloseDoor();
+
         }
     }
 }
