9ac67387078e469f63ce0bce7b9f7eae59b8af91
Lucas Teles <eu@lucasteles.dev>
2021-01-16 10:43:14 -0300

    WIP

---
+++ Slasherlock/Assets/Editor/SurroundingAwarenessEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SurroundingAwareness))]
+public class SurroundingAwarenessEditor : Editor
+{
+    SurroundingAwareness sa;
+
+    public void OnSceneGUI()
+    {
+        sa = target as SurroundingAwareness;
+        if (!sa) return;
+
+        Handles.color = Color.red;
+        var transform = sa.transform;
+        Handles.DrawWireDisc(transform.position, transform.forward, sa.SightRadius);
+    }
+}
