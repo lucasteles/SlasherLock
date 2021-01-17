92ff5dec3c9eb21b078ac4c8d91c92d3f25d8516
Lucas Teles <lucas.teles@outlook.com>
2021-01-17 02:28:00 -0300

    WIP

--- Slasherlock/Assets/Scripts/Sounds/PlayWhenWalk.cs
+++ Slasherlock/Assets/Scripts/Sounds/PlayWhenWalk.cs
@@ -1,4 +1,7 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 [RequireComponent(typeof(AudioSource))]
 public class PlayWhenWalk : MonoBehaviour
@@ -6,25 +9,50 @@ public class PlayWhenWalk : MonoBehaviour
     [SerializeField] Transform player;
     [SerializeField] float minDistance;
     [SerializeField] float minMaxVolumeDistance;
+
+    [SerializeField] float minGrain;
+    [SerializeField] float maxGrain;
+    [SerializeField] float currentGrain;
+
     AudioSource audioSource;
     CheckWalking checkWalking;
-    
-    void Start()
+
+    FilmGrain grain;
+
+    void Awake()
     {
         audioSource = GetComponent<AudioSource>();
-        checkWalking = new CheckWalking(transform);    
+        var volume = FindObjectOfType<Volume>();
+        volume.profile.TryGet(out grain);
+        grain.intensity.value = minGrain;
+    }
+
+    void Start()
+    {
+        checkWalking = new CheckWalking(transform);
     }
 
     void Update()
     {
         var playerDistance = (transform.position - player.position).magnitude;
+
+        if (grain != null)
+            if (playerDistance < minDistance )
+            {
+                var newGrain = (playerDistance * (maxGrain - minGrain)) / (minDistance );
+                grain.intensity.value = maxGrain - newGrain ;// Mathf.Clamp(newGrain, minGrain, maxGrain);
+                currentGrain = (grain.intensity.value);
+            }
+            else
+                grain.intensity.value = minGrain;
+
         if (checkWalking.IsWaking() && playerDistance < minDistance)
         {
             if (playerDistance <= minMaxVolumeDistance)
                 audioSource.volume = 1;
             else
                 audioSource.volume = 1 - (playerDistance / minDistance);
-            
+
             audioSource.PlayIfNotPlaying();
         }
         else if (checkWalking.IsStoppedAndReset())
