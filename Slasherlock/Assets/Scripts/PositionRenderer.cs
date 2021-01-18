50dc4fb4d1b1e877a0a3a13806583570cc4bef5d
Lucas Teles <lucas.teles@outlook.com>
2021-01-18 13:16:15 -0300

    WIP

---
+++ Slasherlock/Assets/Scripts/PositionRenderer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRenderer : MonoBehaviour
+{
+    [SerializeField] int SortingOrderBase = 5000 ;
+    [SerializeField] int Offset;
+    Renderer myRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRenderer.sortingOrder = (int) (SortingOrderBase - transform.position.y - Offset);
+    }
+}
