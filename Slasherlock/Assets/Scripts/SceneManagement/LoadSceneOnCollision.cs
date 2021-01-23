d911947856731f7c0c3f607c4774bbd7628c55aa
Gustavo Passos <theguspassos@gmail.com>
2021-01-23 17:27:26 -0300

    add win condition

---
+++ Slasherlock/Assets/Scripts/SceneManagement/LoadSceneOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.SceneManagement
+{
+    public class LoadSceneOnCollision : MonoBehaviour
+    {
+        [SerializeField] string sceneToLoad;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+            {
+                SceneLoader.Instance.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
