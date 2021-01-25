378e5a6538d4d9ca7a32042b9ce36f162cdea842
Lucas Teles <lucas.teles@outlook.com>
2021-01-25 14:47:37 -0300

    WIP

--- Slasherlock/Assets/Scripts/Ai/Awareness/SurroundingAwareness.cs
+++ Slasherlock/Assets/Scripts/Ai/Awareness/SurroundingAwareness.cs
@@ -31,6 +31,6 @@ public class SurroundingAwareness : MonoBehaviour
         var hitFromRay = Physics2D.Raycast(transform.position, LastTargetFound.position, sightRadius,
             layersToSearchFor | obstacleLayer | playerObstacleLayer);
 
-        return hitFromRay.collider.gameObject.transform == LastTargetFound;
+        return (1 << hitFromRay.collider.gameObject.layer & layersToSearchFor) != 0;
     }
 }
--- Slasherlock/Assets/Scripts/Interactables/Door.cs
+++ Slasherlock/Assets/Scripts/Interactables/Door.cs
@@ -140,7 +140,7 @@ namespace Assets.Interactables.Physics
                     other.gameObject.GetComponent<EnemyFsm>().ResetAfterCloseDoorCoolDown();
 
                 OpenDoor(enemyPlayLockSound);
-                if (enemyPlayLockSound)
+                if (enemyPlayLockSound && !needsKey)
                     Invoke(nameof(EnableEnemyToPlayLockedSound), 1f);
                 enemyPlayLockSound = false;
                 if (CurrentState == State.Locked) ConfirmLockDoor();
